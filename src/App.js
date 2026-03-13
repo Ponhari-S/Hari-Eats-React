fcfd5685a1ec9586daabb770505297232082b698
Ponhari-S <ponhari9923@gmail.com>
2026-03-13 11:22:17 +0530

    DAY-8

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter,RouterProvider } from "react-router";
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
@@ -16,4 +19,16 @@ const AppLayout=()=>{
     )
 }
 
-root.render(<AppLayout />)
+const appRouter=createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout />,
+        errorElement:<Error />
+    },
+    {
+        path:"/about",
+        element:<About />
+    }
+])
+
+root.render(<RouterProvider router={appRouter} />)
