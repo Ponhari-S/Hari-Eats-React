0edf450950983bd4d058252f0b6fcedb349e191a
Ponhari-S <ponhari9923@gmail.com>
2026-04-08 12:51:29 +0530

    Day-20

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,30 @@
+import { useContext } from "react";
+import UserContact from "../utils/UserContact.js";
+
 const Contact = () => {
+    const { user, setUser } = useContext(UserContact);
+
     return (
         <div className="contact">
             <h1>Contact Us</h1>
-            <p>We would love to hear from you! Please reach out to us with any questions, feedback, or inquiries.</p>
+            <p>
+                We would love to hear from you! Please reach out to us with any questions, feedback, or inquiries.
+            </p>
+
+            <label htmlFor="name">Name:</label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Your Name"
+                value={user.name}
+                required
+                onChange={(e) =>
+                    setUser({ ...user, [e.target.name]: e.target.value })
+                }
+            />
         </div>
-    )
-}
+    );
+};
 
 export default Contact;
